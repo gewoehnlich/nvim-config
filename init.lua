dedec511f2a84531c2eec1a13a08bbad26f9349a
gewoehnlich <oomyse@gmail.com>
2025-12-27 13:17:02 +0500

    nvim config

---
+++ init.lua
@@ -0,0 +1,248 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    repo,
+    "--branch=stable",
+    lazypath,
+  }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+  { "williamboman/mason.nvim" },
+  { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+  { "nvimtools/none-ls.nvim" },
+  { import = "plugins" },
+}, lazy_config)
+
+require("mason").setup()
+
+vim.lsp.config("clangd", {
+  cmd = { "clangd", "--background-index", "--clang-tidy" },
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+  init_options = {
+    clangdFileStatus = true,
+    usePlaceholders = true,
+    completeUnimported = true,
+  },
+  root_dir = require("lspconfig.util").root_pattern(
+    "compile_commands.json",
+    "compile_flags.txt",
+    ".git"
+  ),
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+})
+
+vim.lsp.config("pyright", {
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "strict",
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "workspace",
+      },
+    },
+  },
+})
+
+require("nvim-treesitter.configs").setup {
+  ensure_installed = {
+    "python",
+    "yaml",
+  },
+  highlight = { enable = true },
+  indent = { enable = true },
+}
+
+vim.lsp.config("intelephense", {
+  settings = {
+    intelephense = {
+      format = {
+        braces = "psr12",
+        enable = true,
+      },
+    },
+  },
+})
+vim.lsp.enable "intelephense"
+
+local none_ls = require "null-ls"
+none_ls.setup {
+  sources = {
+    none_ls.builtins.diagnostics.phpcs.with {
+      filetypes = { "php" },
+      extra_args = {
+        "--standard=PSR12",
+        -- "--sniffs=Generic.Files.LineLength",
+        -- "--runtime-set", "lineLimit", "80",
+        -- "--runtime-set", "absoluteLineLimit", "0"
+      },
+    },
+    none_ls.builtins.diagnostics.mypy.with {
+      command = "./.venv/bin/mypy",
+      args = {
+        "--show-column-numbers",
+        "$FILENAME",
+      },
+    },
+    none_ls.builtins.diagnostics.pylint.with {
+      command = "./.venv/bin/pylint",
+      args = {
+        "--from-stdin",
+        "$FILENAME",
+        "-f",
+        "json",
+        "--enable=all",
+      },
+      to_stdin = true,
+    },
+  },
+}
+
+require("conform").setup {
+  formatters_by_ft = {
+    html = { "prettierd" },
+    css = { "prettierd" },
+    javascript = { "prettierd" },
+    typescript = { "prettierd" },
+    json = { "prettierd" },
+    yaml = { "prettierd" },
+    markdown = { "prettierd" },
+  },
+}
+
+require("nvim-tree").setup {
+  filters = {
+    dotfiles = false,
+    custom = {},
+  },
+  git = {
+    enable = true,
+    ignore = false,
+  },
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "vue" },
+  callback = function()
+    vim.defer_fn(function()
+      vim.opt.commentstring = "<!-- %s -->"
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.tabstop = 4
+      vim.opt_local.softtabstop = 4
+      vim.opt_local.expandtab = true
+      vim.opt_local.autoindent = true
+      vim.opt_local.smartindent = true
+    end, 10)
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "php" },
+  callback = function()
+    vim.opt.commentstring = "// %s"
+    vim.bo.shiftwidth = 4
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = true
+    vim.bo.autoindent = true
+    vim.bo.smartindent = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "yaml" },
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+    vim.bo.autoindent = true
+    vim.bo.smartindent = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "Makefile" },
+  callback = function()
+    vim.bo.shiftwidth = 4
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = false
+  end,
+})
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+local vue_language_server_path = vim.fn.stdpath "data"
+  .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+local tsserver_filetypes = { "typescript", "javascript", "vue" }
+local vue_plugin = {
+  name = "@vue/typescript-plugin",
+  location = vue_language_server_path,
+  languages = { "vue" },
+  configNamespace = "typescript",
+}
+local ts_ls_config = {
+  init_options = {
+    plugins = {
+      vue_plugin,
+    },
+  },
+  filetypes = tsserver_filetypes,
+}
+
+local vue_ls_config = {}
+
+vim.lsp.config("vue_ls", vue_ls_config)
+vim.lsp.config("ts_ls", ts_ls_config)
+vim.lsp.enable { "ts_ls", "vue_ls" }
+
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
---
+++ lua/options.lua
@@ -0,0 +1,8 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+vim.o.cursorlineopt = 'both' -- to enable cursorline!
+vim.opt.colorcolumn = "80"
+vim.opt.relativenumber = true;
